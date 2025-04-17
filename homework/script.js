1fe1855a79c4ff332f7e899a04f2f00409060651
Develux <develux@ip-192-168-68-112.us-west-2.compute.internal>
2025-04-17 09:39:01 +0300

    first homework

---
+++ homework/script.js
@@ -0,0 +1,62 @@
+
+
+// частина 2, змінні, завдання 1
+let name = "Yevhen";
+console.log(name);
+
+// частина 2, змінні, завдання 2
+// let birthYear = 1983
+// let currentYear = new Date().getFullYear();
+// let age = currentYear - birthYear;
+// console.log(age);
+
+// частина 2, змінні, завдання 3
+// let a = 5;
+// let b = 10;
+// let c = a;
+// a = b;
+// b = c;
+// console.log(a);
+// console.log(b);
+
+
+// частина 2, типи даних, завдання 1
+let num = 5
+console.log(typeof num);
+let string = 'string'
+console.log(typeof string);
+const boolTrue = true;
+console.log(typeof boolTrue);
+let obj = {};
+console.log(typeof obj);
+let undValue = undefined;
+console.log(undValue);
+let nullValue = null;
+console.log(nullValue);
+
+// частина 2, типи даних, завдання 2
+let a = "5";
+let b = 3;
+let result = Number(a)+ b;
+console.log(result);
+
+// частина 2, умовні оператори, завдання 1
+// let number = prompt('Вводь число бігом!');
+// number % 2 === 0 ? alert("Парне!") : alert("Не парне!");
+
+// частина 2, умовні оператори, завдання 2
+// let age = prompt('Введи свій вік!');
+// if (age >= 18){
+//     alert('Ласкаво просимо пане!');
+// } else {
+//     alert('Доступ заборонено шмаркля!');
+// }
+
+// частина 2, умовні оператори, завдання 3
+// let password = 'qwerty';
+// let userData = prompt('Введіть свій пароль');
+// if(userData === password){
+//     alert('Успішний вхід');
+// } else{
+//     alert('Невірний пароль');
+// };
