ab9f25fdd986f6aaa4c22490ebf2d63d4cc75678
Yevhen <mazuren83@gmail.com>
2025-04-24 12:02:37 +0300

    Homework part 2

---
+++ homework/script2.js
@@ -0,0 +1,67 @@
+
+// частина 3 Задачі  &&, ||, ! 
+
+// задача 1 
+// let isAdmin = true;
+// let isLoggedIn = false;
+// let isUser = true;
+
+// if (isAdmin || isLoggedIn || isUser) {
+//     console.log("Доступ дозволено");
+// }
+
+
+// задача 2
+// let age = 17;
+// let ageData = prompt('Введіть свій вік');
+// if (ageData >= 18) {
+//     alert("Можна купити алкоголь");
+// } else{
+//     alert("Заборонено")
+// };
+
+// задача 3
+// let login = 'admin';
+// let password = '12345';
+
+// if (!login && password) {
+//     console.log("Доступ дозволено");
+// } else{
+//     console.log("Доступ заборонено");
+// }
+
+//задача 4
+// let number = 15;
+// let numberData = prompt("Введіть будь-яке число");
+// if (numberData >= 10 && numberData <= 20) {
+//     alert("У межах");
+// } else{
+//     alert("Поза межами");
+// }
+
+// задача 5
+// let name = 'Zheka';
+
+// if (name) {
+//   console.log(`Привіт, ${name}`);
+// } else {
+//   console.log("Ім’я не вказано");
+// }
+
+// задача 6
+let number = 6;
+if (number > 0 && number % 2 === 0) {
+    console.log("Число парне і додатне");
+  }
+
+// задача 7
+// let day = 'Saturday';
+
+// if (day = day || day == 'Sunday') {
+//     console.log("Вихідний");
+// }
+
+// задача 8
+// let isLoggedIn = false;
+// if (!isLoggedIn) {console.log("Будь ласка, увійдіть")};
+
